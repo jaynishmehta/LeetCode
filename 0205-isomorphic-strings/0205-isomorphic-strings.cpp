8873222c446208bf07e06172ad9c54fb8a39d012
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2024-04-02 11:14:56 +0530

    Time: 6 ms (65.28%), Space: 8.3 MB (72.86%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,31 @@
+// class Solution {
+// public:
+//     bool isIsomorphic(string s, string t) {
+//         unordered_map<char,int>m1;
+//         unordered_map<char,int>m2;
+//         if(s.size()!=t.size())return false;
+        
+//     }
+// };
+class Solution {
+public:
+    bool isIsomorphic(string str1, string str2) {
+        int m=str1.length();
+        int n=str2.length();
+        int freq1[256]={0};
+        int freq2[256]={0};
+        if(m!=n)
+            return false;
+        for(int i=0;i<n;i++)
+        {
+            if(freq1[str1[i]]==0 && freq2[str2[i]]==0)
+            {
+                freq1[str1[i]]=str2[i];
+                freq2[str2[i]]=str1[i];
+            }
+            else if(freq1[str1[i]]!=str2[i])
+                return false;
+        }
+        return true;
+    }
+};
