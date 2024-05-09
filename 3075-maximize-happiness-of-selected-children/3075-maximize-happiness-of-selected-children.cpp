c6b8bee8b018c62246243fb7290cc9fed064405e
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2024-05-09 17:54:52 +0530

    Time: 163 ms (52.83%), Space: 107.1 MB (60.15%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -9,12 +9,9 @@ public:
             // sum+=(happiness[i-1]-i);
             if((happiness[i-1]-i)<0){
                 sum+=0;
-                cout<<"a"<<happiness[i-1]<<endl;
             }
             else{
                 sum+=(happiness[i-1]-(i-1));
-                cout<<"b"<<happiness[i-1]<<endl;
-                cout<<"sum"<<sum<<endl;
             }
             i++;
         }
