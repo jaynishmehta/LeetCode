03cbb83bcfe1956a784e4b3a2d35d56f72c45d56
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2024-06-06 15:57:02 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (5.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
-     int f(int n,vector<int>&dp){
-        // if(n<=1)return 1;
-         if(n==1)return 1;
-         if(n==0)return 1;
-        if(dp[n]!=-1)return dp[n];
-        dp[n]=f(n-1,dp)+f(n-2,dp);
-        return dp[n];
-    }
-    
 public:
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return f(n,dp);
+        if (n <= 1) return 1; // Base case: 1 way to climb 0 or 1 step
+        // inintialize the array do not give -1 just the size;
+        vector<int> dp(n + 1); // Initialize the dp array
+        dp[0] = 1; // 1 way to stay at the ground (doing nothing)
+        dp[1] = 1; // 1 way to reach the first step
+
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2]; // Ways to reach the ith step
+        }
+
+        return dp[n]; 
     }
 };
