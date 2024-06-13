2ea0dc62248876d22d78e6600e9389837ba6b263
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2024-06-13 14:54:50 +0530

    Time: 9 ms (27.44%), Space: 27.4 MB (11.35%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,28 +1,24 @@
 class Solution {
-public:
-    int helper(int i, int j,vector<vector<int>>& oG,vector<vector<int>> &dp,int m, int n)
-    {
-            if(i >= m || j >= n)
-            {
-                return 5100;
-            }
-
-            if(i == m-1 && j == n-1)
-            {
-                return oG[i][j];
-            }
-            if(dp[i][j]!=-1)return dp[i][j];
-            int p1 = oG[i][j] + helper(i+1,j,oG,dp,m,n);
-            
-            int p2 = oG[i][j]+helper(i,j+1,oG,dp,m,n);
-
-            return dp[i][j]=min(p1,p2);
-
+    //WHEN LIMIT EXCEEDS WE RETURN INT_MAX INSTEAD TO 0;
+    int f(int m,int n,vector<vector<int>>& grid,int row,int col,vector<vector<int>>&dp){
+        if(row==m-1 && col==n-1){
+            return grid[row][col];
+        }
+        if(row>=m || col>=n)return INT_MAX;
+        if(dp[row][col]!=-1)return dp[row][col];
+        
+        int right = (col+1<n)? grid[row][col]+f(m,n,grid,row,col+1,dp):INT_MAX;
+        int down = (row+1<m)? grid[row][col]+f(m,n,grid,row+1,col,dp):INT_MAX;
+        
+       
+        return dp[row][col]=min(right,down);
     }
-    int minPathSum(vector<vector<int>>& oG) {
-        int m = oG.size();
-        int n = oG[0].size();
-        vector<vector<int>> dp(200,vector<int> (200,-1));
-        return helper(0,0,oG,dp,m,n);
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        // int sum=0;
+        int m= grid.size();
+        int n=grid[0].size();
+        vector<vector<int>>dp(200,vector<int>(201,-1));  //dp-1
+        return f(m,n,grid,0,0,dp);
     }
 };
