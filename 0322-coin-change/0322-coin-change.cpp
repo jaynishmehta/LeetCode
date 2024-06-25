ea78bd89063be53ef33b44c49522b0b20489044f
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2024-06-25 15:28:32 +0530

    Time: 56 ms (17.60%), Space: 80.5 MB (5.01%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    int MAX = 1e7+7;
+    int f(int i, int sum, vector<int>& coins, vector<vector<long long int>>& dp) {
+        if (sum == 0) {
+            return 0;
+        }
+        if (i < 0) {
+            return MAX;
+        }
+        
+        if (dp[i][sum] != -1) {
+            return dp[i][sum];
+        }
+        
+        long long int notpick = f(i - 1, sum, coins, dp);
+        
+        long long int pick = MAX;
+        if (coins[i] <= sum) {
+            pick = 1+f(i, sum - coins[i], coins, dp);
+            // if (res != INT_MAX) {
+            //     pick = res + 1;
+            // }
+        }
+        
+        dp[i][sum] = min(pick, notpick);
+        return dp[i][sum];
+    }
+
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<long long int>> dp(n, vector< long long int>(amount + 1, -1));
+        long long int result = f(n - 1, amount, coins, dp);
+        return result == MAX ? -1 : result;
+    }
+};
