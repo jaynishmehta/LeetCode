0e9e22097fa2b26c927c2749e43890e8894e9dd7
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2024-07-10 12:03:54 +0530

    Time: 4 ms (67.13%), Space: 14 MB (41.82%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int ans=0;
+        for(auto it:logs){
+            if(it=="./"){
+                ans+=0;
+            }
+            else if(it=="../"&&ans!=0){
+                ans-=1;
+            }
+            else if(it=="../"&&ans==0){
+                ans+=0;
+            }
+            else{
+                ans+=1;
+            }
+        }
+       
+        return ans;
+    }
+};
