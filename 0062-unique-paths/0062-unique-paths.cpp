11eb6351bc1b8873a722e8e414642031a0131d31
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2024-08-03 00:14:13 +0530

    Time: 0 ms (100.00%), Space: 8 MB (8.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,23 +1,23 @@
 class Solution {
-    int solve(int i,int j,vector<vector<int>>&dp){
-        if(i<0 || j<0)return 0;
-        if(i==0 && j==0){
-            return 1;
-        }
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-        int right=0,down=0;
-        if(j>0){
-            right= solve(i,j-1,dp);
-        }
-        if(i>0){
-            down = solve(i-1,j,dp);
-        }
-        return dp[i][j]= right+down;
-    }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n+1,-1));
-        return solve(m-1,n-1,dp);
+        vector<vector<int>>dp(m,vector<int>(n+1,0));
+        dp[0][0]=1;   //bottom to top approach ,, base case 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)dp[0][0]=1;  // base case
+                else{           //copy the memoization ref
+                    int right=0,down=0; 
+                    if(j>0){
+                        right= dp[i][j-1];
+                    }
+                    if(i>0){
+                        down = dp[i-1][j];
+                    }
+                    dp[i][j]= right+down;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
