e78f2d55d20c15ed90b5eddcd651adf7d2541982
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2024-09-02 02:58:09 +0530

    Time: 8 ms (77.44%), Space: 14.7 MB (11.71%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> v;
+        // sort(nums.begin(),nums.end());
         map<int,int>m;
+        vector<int>ans;
         for(int i=0;i<nums.size();i++){
-            int complement=target-nums[i];
-            if (m.find(complement) != m.end()) {
-                // Found a pair of indices that add up to the target
-                v.push_back(m[complement]); // Index of complement
-                v.push_back(i); // Current index
-                break; // We found the answer, so exit the loop
+            if(m.find(target-nums[i])!=m.end()){
+                ans.push_back(i);
+                ans.push_back(m[target-nums[i]]);
+                return ans;
             }
-
-            // Store the current number and its index in the map
-            m[nums[i]] = i;
+            else m[nums[i]]=i;
         }
-        return v;
+        return ans;
+        
     }
 };
