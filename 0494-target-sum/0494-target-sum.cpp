be9d261aecebffd77fc4dd5160f37e01f339a9dd
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2024-12-26 08:16:34 +0530

    Time: 7 ms (71.87%), Space: 15.2 MB (45.09%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,4 +1,4 @@
-//this question can be solve using  as number of subsets(s1,s1) whose abs(diff) is equal to target (dp-18 striver)
+//this question can be solve using  as number of subsets(s1,s2) whose abs(diff) is equal to target (dp-18 striver)
 class Solution {
     int f(int i,int s,vector<int>&nums,vector<vector<int>>&dp){
         if(i<0){
@@ -25,7 +25,20 @@ public:
         if((sum-target)<0 || (sum-target)%2!=0)return 0;
         int x= (sum-target)/2;
         int n= nums.size();
-        vector<vector<int>>dp(n,vector<int>(x+1,-1));
-        return f(n-1,(sum-target)/2,nums,dp);
+        vector<vector<int>>dp(n,vector<int>(x+1,0));
+        // return f(n-1,(sum-target)/2,nums,dp);  // memoization (dp-18_lec) s1+s2=s, s1-s2=target; s2= (s-target)/2  ,, find number of such s2 subsequence exist;
+       
+        dp[0][0] = 1;  // there's one way to get sum 0 with no elements
+        if (nums[0] <= x) dp[0][nums[0]] += 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= x; j++) {
+                int notPick = dp[i - 1][j];
+                int pick = (j >= nums[i]) ? dp[i - 1][j - nums[i]] : 0;
+                dp[i][j] = pick + notPick;
+             }
+        }
+    
+    return dp[n - 1][x];
     }
 };
