24321b606d33387594a502a1aa94ed4b583340a1
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2025-01-30 13:27:44 +0530

    Time: 26 ms (63.66%), Space: 27.3 MB (66.29%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,37 +1,30 @@
 class Solution {
-    int bond(int i,int j,string &t1,string &t2,vector<vector<int>>&dp){
-        if(i<0 || j<0)return 0;
-        
+public:
+    int solve(int i,int j,string &t1,string &t2,vector<vector<int>>&dp){
+        if(i>=t1.size() || j>= t2.size())return 0;
         if(dp[i][j]!=-1)return dp[i][j];
-        
-        
+        int pick=0,not_pick=0;
         if(t1[i]==t2[j]){
-            return dp[i][j]= 1 + bond(i-1,j-1,t1,t2,dp);
+            pick= 1+ solve(i+1,j+1,t1,t2,dp);
         }
         else{
-            return dp[i][j]=max(bond(i-1,j,t1,t2,dp),bond(i,j-1,t1,t2,dp));
+            not_pick= max(solve(i+1,j,t1,t2,dp),solve(i,j+1,t1,t2,dp));
         }
+        return dp[i][j]=pick+not_pick;
     }
-public:
-    int longestCommonSubsequence(string t1, string t2) {
-        int n= t1.size();
-        int m= t2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++)dp[i][0]=0;
-        for(int i=0;i<=m;i++)dp[0][i]=0;
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(t1[i-1]==t2[j-1]){
-                    dp[i][j]= 1+dp[i-1][j-1];
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>>dp(text1.size()+1,vector<int>(text2.size()+1,0));
+        // return solve(0,0,text1,text2,dp);
+        for(int i=1;i<=text1.size();i++){
+            for(int j=1;j<=text2.size();j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]= 1+ dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        
-        // return bond(n-1,m-1,t1,t2,dp);
-        return dp[n][m];
+        return dp[text1.size()][text2.size()];
     }
 };
