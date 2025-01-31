e1bddeb945200cf19140c049c85fd1179431dca2
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2025-02-01 01:21:58 +0530

    Time: 0 ms (100%), Space: 7.8 MB (29.47%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        vector<int>v;
-        while(n>0){
-            v.push_back(n&1);
-            n= n>>1;
+        int result=0;
+        for(int i=0;i<32;i++){
+            int bit= n&1;
+            result = (result <<1)|bit;   // Append the bit to the result
+            n=n>>1;
         }
-        int x= 32-v.size();
-        for(int i=0;i<x;i++)v.push_back(0);
-        long long ans=0;
-        reverse(v.begin(),v.end());
-        for(int i=0;i<v.size();i++){
-            cout<<v[32-i-1]<<" ";
-            ans+=(v[i]*pow(2,i));
-        }
-        return ans;
+        return result;
     }
 };
