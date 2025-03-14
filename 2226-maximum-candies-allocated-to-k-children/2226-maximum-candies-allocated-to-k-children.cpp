85cc4fcd71ac99ad90846f51bf1f8a60806d7351
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2025-03-14 21:45:34 +0530

    Time: 31 ms (45.11%), Space: 88.2 MB (73.33%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,72 @@
+// class Solution {
+// public:
+//      bool check(long long mid,vector<int>& c,long long k){
+//         if (mid == 0) return false;  
+//         int cnt=0;
+//         for(int i=0;i<c.size();i++){
+//             if(c[i]>mid){
+//                 cnt+= (c[i]/mid);
+//                  if (cnt >= k) return true;
+//             }
+//         }
+//         if(cnt>=k)return true;
+//         else return false;
+//      }
+//     int maximumCandies(vector<int>& c, long long k) {
+//         long long sum = accumulate(c.begin(), c.end(), 0LL);
+//         if (sum < k) return 0;
+//         long long maxi=0;
+//         for(int i=0;i<c.size();i++){
+//             if(c[i]>maxi)maxi=c[i];
+//         }
+        
+//         long long mid=0;
+//         long long int l=1;
+//         long long r= maxi;
+//         long long ans=0;
+//         while(l<=r){
+//             mid = (l+(r-l)/2);
+//             cout<<mid;
+//             if(check(mid,c,k)){
+//                 ans= mid;
+//                 l= mid+1;
+//             }
+//             else{
+//                 r= mid-1;
+//                 // ans= mid;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+class Solution {
+public:
+    bool check(long long mid, vector<int>& c, long long k) {
+        if (mid == 0) return false;  // Prevent division by zero
+        long long cnt = 0;
+        for (int i = 0; i < c.size(); i++) {
+            cnt += (c[i] / mid);  // Always count how many children can receive `mid` candies
+            if (cnt >= k) return true;  // Early exit if condition is met
+        }
+        return false;  // Return false if we couldn't distribute enough
+    }
+
+    int maximumCandies(vector<int>& c, long long k) {
+        long long sum = accumulate(c.begin(), c.end(), 0LL);
+        if (sum < k) return 0;  // Not enough candies to give at least one to each child
+
+        long long l = 1, r = *max_element(c.begin(), c.end()), ans = 0;
+
+        while (l <= r) {
+            long long mid = l + (r - l) / 2;
+            if (check(mid, c, k)) {
+                ans = mid;  // Store valid `mid`
+                l = mid + 1;  // Try for a larger `mid`
+            } else {
+                r = mid - 1;  // Try a smaller `mid`
+            }
+        }
+        return ans;
+    }
+};
