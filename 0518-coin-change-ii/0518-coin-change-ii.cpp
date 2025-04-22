9da646324b038f68dc8011b1e19b9d0b6bab4280
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2025-04-22 23:29:24 +0530

    Time: 23 ms (46.51%), Space: 28.2 MB (66.92%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,39 +1,33 @@
 class Solution {
-    int f(int i,int t,vector<int>&coins,vector<vector<int>>&dp){
-        if(i<0){
-            if(t==0)return 1;
-            else return 0;
-        }
-        
-        if(dp[i][t]!=-1)return dp[i][t];
-        int pick=0;
-        if(t>=coins[i]){
-           pick= f(i,t-coins[i],coins,dp);
-        }
-        int notpick= f(i-1,t,coins,dp);
-        
-        return dp[i][t]= pick+notpick;
-    }
 public:
-    int change(int amount, vector<int>& coins) {
-        int n= coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
-        // return f(n-1,amount,coins,dp);
-        for (int i = 0; i <= amount; i++) {
-        if (i % coins[0] == 0)
-            dp[0][i] = 1; 
-            // At ind==0, we are considering the first element, if the target value is                                   divisible by the first coin’s value, we set the cell's value as 1 or else 0.
+    int f(int i,int amount,vector<vector<int>>& dp,vector<int>& coins){
+        if(i==coins.size()){
+            return amount==0;
         }
-        for(int i=1;i<n;i++){
+        if(dp[i][amount]!=-1)return dp[i][amount];
+        int pick =0;
+        int notpick = f(i+1,amount,dp,coins);
+        if(amount>=coins[i]){
+            pick = f(i,amount-coins[i],dp,coins);
+        }
+        return dp[i][amount]= pick+notpick;
+    }
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,0));
+        // return f(0,amount,dp,coins);
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)dp[0][i]=1;
+        }
+        for(int i=1;i<coins.size();i++){
             for(int j=0;j<=amount;j++){
-                int pick=0;
-                if(j>=coins[i]){
-                   pick= dp[i][j-coins[i]];
+                long long int notpick= dp[i-1][j];
+                long long int pick=0;
+                if(coins[i]<=j){
+                    pick= dp[i][j-coins[i]];
                 }
-                int notpick= dp[i-1][j];
-                dp[i][j]=pick+notpick;
+                dp[i][j]= pick+notpick;
             }
         }
-        return dp[n-1][amount];
+        return dp[coins.size()-1][amount];
     }
 };
