64d939f987d14df4a9bafc2112f969597f32d48f
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2025-10-14 10:04:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        int c1=1;
+        if(nums.size()==2){
+            if(nums[0]!=nums[1])return true;
+            else return false;
+        }
+        for(int i=0;i<nums.size()-k-1;i++){
+            if((nums[i+1]-nums[i]>0)&&(nums[i+k+1]-nums[i+k]>0)){
+                c1++;
+                if(c1==k)return true;
+            }
+            else c1=1;
+        }
+        return false;
+    }
+};
