c6912fcba41ced713730cbfcf0eb9a27ca4f4361
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2025-11-03 20:14:23 +0530

    Time: 3 ms (72.07%), Space: 99.9 MB (65.59%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int minCost(string c, vector<int>& t) {
-        int n=c.size();
+        long long int n=c.size();
         // vector<vector<int>>dp(n,vector<int>(n+1,-1));
         // return solve()
-        int i=1;
-        int cnt=0;
-        char prev= 0;
+        long long int i=1;
+        long long int cnt=0;
+        long long int prev= 0;
         while(i<n){
             if(c[i]==c[prev]){
-                int ind=0;
+                long long int ind=0;
                 if(t[i]<t[prev]){
                     ind=i;
                     i++;
