e051d76c6ca74a83e17027429eb315824fd48296
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2025-11-22 11:05:59 +0530

    Time: 4 ms (1.02%), Space: 22.1 MB (100%) - LeetHub

--- 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
+        bool ans=false;
         int cnt=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%3!=0)cnt++;
+        while(ans!=true){
+            for(int i=0;i<nums.size();i++){
+                int x= nums[i]%3;
+                if(x==1){
+                    nums[i]--;
+                    cnt+=1;
+                }
+                else if(x==2){
+                    nums[i]++;
+                    cnt+=1;
+                }
+            }
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]%3!=0)ans=false;
+                else {
+                    ans=true;
+                }
+            }
         }
         return cnt;
     }
