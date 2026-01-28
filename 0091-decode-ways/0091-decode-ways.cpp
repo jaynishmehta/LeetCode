6a0ae3abe528a54fcd8a6a31e4833aaf8ccb2b49
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2026-01-29 00:21:05 +0530

    Time: 3 ms (18.65%), Space: 10.9 MB (15.06%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int solve(int i,string s,vector<int>&dp,int n){
-        if(i==n)return 1;
-        if((s[i]-'0')==0){
-            // i++;
-            return 0;
-        }
-        // if(i>n)return 0;
-        
+    int solve(int i,string s,vector<int>&dp){
+        if(i==s.size())return 1;
+
+        if(s[i]=='0')return 0;
         if(dp[i]!=-1)return dp[i];
-        int take1= solve(i+1,s,dp,n);
-        int take2=0;
-        if(i+1<n && (s[i]=='1' || (s[i]=='2' && s[i+1]-'0'<=6))){
-            take2=solve(i+2,s,dp,n);
+
+        int take1 = solve(i+1,s,dp);
+        int take2= 0;
+
+        if(i+1<s.size() && (s[i]=='1' || s[i]=='2' && s[i+1]-'0'<7)){
+            take2= solve(i+2,s,dp);
         }
-        // maxi= max(take1+take2,maxi);
         return dp[i]=take1+take2;
-
+        
     }
-
     int numDecodings(string s) {
-        vector<int>dp(s.size(),-1);
-        int n= s.size();
-        return solve(0,s,dp,n);
+        vector<int>dp(101,-1);
+        return solve(0,s,dp);
     }
 };
