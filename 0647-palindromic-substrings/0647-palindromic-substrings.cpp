dd591b591009c4ffc4e4f4ecae9450ccec0041f4
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2026-01-28 20:31:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,25 +1,52 @@
+// class Solution {
+// public:
+//     bool solve(string &s,int i,int j,vector<vector<int>>&dp){
+//         if(i>=j){
+//             return 1;
+//         }
+//         if(dp[i][j]!=-1){
+//             return dp[i][j];
+//         }
+//         if(s[i]==s[j]) return solve(s,i+1,j-1,dp);
+//         return 0;
+//     }
+//     string longestPalindrome(string s) {
+//         int n= s.size();
+//         vector<vector<int>>dp(s.size(),vector<int>(s.size()+1,-1));
+//         int maxlen=INT_MIN;
+//         int s_index=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 if(solve(s,i,j,dp)==true){
+//                     if(j-i+1>maxlen){
+//                         maxlen=j-i+1;
+//                         s_index=i;
+//                     }
+//                 }
+//             }
+//         }
+//         return s.substr(s_index,maxlen);
+//     }
+// };
 class Solution {
 public:
+    int is_palin(int i,int j,string s,vector<vector<int>>&dp){
+        if(i>=j)return 1;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s[i]==s[j])return is_palin(i+1,j-1,s,dp);
+        return 0;
+    }
     int countSubstrings(string s) {
+        int n= s.size();
+        vector<vector<int>>dp(s.size(),vector<int>(s.size()+1,-1));
+        int maxlen=INT_MIN;
+        int s_index=0;
         int cnt=0;
-        int l=0,r=0;
-        int l_pointer=0;
         for(int i=0;i<s.size();i++){
-            // odd palindrome
-            l=i,r=i;
-            while(l>=0 && r<s.size() && s[l]==s[r]){
-                cnt++;
-                l_pointer=l;
-                l--;
-                r++;
-            }
-            // even palindrome
-            l=i,r=i+1;
-            while(l>=0 && r<s.size() && s[l]==s[r]){
-                cnt++;
-                l_pointer=l;
-                l--;
-                r++;
+            for(int j=i;j<s.size();j++){
+                if(is_palin(i,j,s,dp)){
+                    cnt++;
+                }
             }
         }
         return cnt;
