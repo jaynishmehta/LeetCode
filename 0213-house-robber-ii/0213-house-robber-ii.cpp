6228274b1d5db1095394e0fd302d8085eba88c9f
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2026-01-29 11:25:37 +0530

    Time: 4 ms (1.23%), Space: 15.3 MB (7.65%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,21 +1,18 @@
 class Solution {
-    int f(int i,int end,vector<int>&nums,vector<int>&dp){
-        if(i>end)return 0;
+    int solve(vector<int>&dp,int i,int j,vector<int>nums){
+        if(i>j)return 0;
         if(dp[i]!=-1)return dp[i];
-        
-        int pick= nums[i]+f(i+2,end,nums,dp);
-        int not_pick =f(i+1,end,nums,dp);
-         
-        return dp[i] = max(pick,not_pick);
+        int take= nums[i]+solve(dp,i+2,j,nums);
+        int nottake= solve(dp,i+1,j,nums);
+        return dp[i]= max(take,nottake);
     }
 public:
+
     int rob(vector<int>& nums) {
         int n= nums.size();
         if(n==1)return nums[0];
-        vector<int>dp1(101,-1);
-        vector<int>dp2(101,-1);
-        int include=f(0,n-2,nums,dp1);
-        int exclude=f(1,n-1,nums,dp2);
-        return max(include,exclude);
-    }   
+        vector<int>dp1(nums.size()+1,-1);
+        vector<int>dp2(nums.size()+1,-1);
+        return max(solve(dp1,0,n-2,nums),solve(dp2,1,n-1,nums));
+    }
 };
