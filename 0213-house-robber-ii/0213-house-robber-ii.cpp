58c89e0daa557312a8f350f3ea3f590a44f2d1fa
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2026-01-29 11:24:08 +0530

    Time: 0 ms (100%), Space: 9.7 MB (100%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,41 +1,21 @@
 class Solution {
-    int solve(int i,vector<int>&dp,vector<int>&nums,int n){
-        if(i>=n)return 0;
+    int f(int i,int end,vector<int>&nums,vector<int>&dp){
+        if(i>end)return 0;
         if(dp[i]!=-1)return dp[i];
         
-        int rob= nums[i]+solve(i+2,dp,nums,n);
-        
-        int not_rob= 0+solve(i+1,dp,nums,n);
-        
-        return dp[i]= max(rob,not_rob);
+        int pick= nums[i]+f(i+2,end,nums,dp);
+        int not_pick =f(i+1,end,nums,dp);
+         
+        return dp[i] = max(pick,not_pick);
     }
 public:
     int rob(vector<int>& nums) {
-        // vector<int>dp1(nums.size(),-1);
-        // vector<int>dp2(nums.size(),-1);
-        int n = nums.size();
+        int n= nums.size();
         if(n==1)return nums[0];
-        // int m1= solve(0,dp1,nums,n-1);
-        // int m2= solve(1,dp2,nums,n);
-        // return max(m1,m2);
-        vector<int>dp1(n+1,0);
-        vector<int>dp2(n+1,0);
-        dp1[0]=0;
-        dp1[1]=nums[0];
-        for(int i=2;i<=n-1;i++){
-            int rob= nums[i-1]+dp1[i-2];
-            int not_rob= 0+dp1[i-1];
-            dp1[i]= max(rob,not_rob);
-        }
-        int m1= dp1[n-1];
-        dp2[0]=0;
-        dp2[2]=nums[1];
-        for(int i=3;i<=n;i++){
-            int rob= nums[i-1]+dp2[i-2];
-            int not_rob= 0+dp2[i-1];
-            dp2[i]= max(rob,not_rob);
-        }
-        int m2= dp2[n];
-        return max(m1,m2);
-    }
+        vector<int>dp1(101,-1);
+        vector<int>dp2(101,-1);
+        int include=f(0,n-2,nums,dp1);
+        int exclude=f(1,n-1,nums,dp2);
+        return max(include,exclude);
+    }   
 };
