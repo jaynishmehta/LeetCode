d597269b69fdbdc422c56daf6dfd297c33da1ac1
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2026-01-30 12:33:48 +0530

    Time: 0 ms (100%), Space: 17.9 MB (23.08%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,15 +1,44 @@
+// class Solution {
+//     int solve(int i,int n,vector<int>&dp,vector<int>&nums,int maxi){
+//         if(i==n){
+//             return 1;
+//             }
+//         if(dp[i]!=-1)return dp[i];
+//         int take= nums[i]*solve(i+1,n,dp,nums,maxi);
+//         // int not_take= 0;
+        
+//         maxi= max(take,maxi);
+//         return dp[i]=maxi;
+//     }
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         vector<int>dp(nums.size(),-1);
+//         int n= nums.size();
+//         int maxi=0;
+//         return solve(0,n,dp,nums,maxi);
+//     }
+// };
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxi=nums[0];
-        for(int i=0;i<nums.size()-1;i++){
-            int q= nums[i];
-            int p=max(nums[i],nums[i+1]);
-            maxi= max(maxi,p);
-            for(int j=i+1;j<nums.size();j++){
-                q*=nums[j];
-                if(maxi<q)maxi=q;
-            }
+        int maxi = INT_MIN;
+        int prod=1;
+
+        for(int i=0;i<nums.size();i++)
+        {
+          prod*=nums[i];
+          maxi=max(prod,maxi);
+          if(prod==0)
+           prod=1;
+        }
+        prod=1;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+          prod*=nums[i];
+
+          maxi=max(prod,maxi);
+          if(prod==0)
+           prod=1;
         }
         return maxi;
     }
