7b033d103aa2cd65995ffb5557b321c003472d66
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2026-02-01 18:42:45 +0530

    Time: 35 ms (33.02%), Space: 27.1 MB (46.9%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,25 +1,23 @@
+
 class Solution {
     const int mod = 1e9 + 7;
-   int bond(int i,int j,string &s, string &t,vector<vector<int>>&dp){
-        if(j<0)return 1;
-        if(i<0)return 0;
-        
+    int solve(int i,int j,string &s,string & t,vector<vector<int>>&dp){
+        if(j==t.size())return 1;
+        if(i==s.size())return 0;
+
         if(dp[i][j]!=-1)return dp[i][j];
         
+        long long take=0;
         if(s[i]==t[j]){
-            return dp[i][j]= ((bond(i-1,j-1,s,t,dp))%mod+ (bond(i-1,j,s,t,dp))%mod)%mod;  
-                                            // this sinifiessearch for t[j] from the last of i in string s
+            take= solve(i+1,j+1,s,t,dp)%mod;
         }
-        else{   // if not found reduce the search space by 1 in s
-            return dp[i][j]= (bond(i-1,j,s,t,dp))%mod;
-        }
+        long long int nottake= solve(i+1,j,s,t,dp)%mod;
+        return dp[i][j]=(take%mod+nottake%mod)%mod;
     }
 public:
+
     int numDistinct(string s, string t) {
-        int n=s.size();
-        int m= t.size();
-        vector<vector<int>>dp(n,vector<int>(m+1,-1));
-        
-        return bond(n-1,m-1,s,t,dp);
+        vector<vector<int>>dp(s.size()+1,vector<int>(t.size()+1,-1));
+        return solve(0,0,s,t,dp)%mod;
     }
 };
