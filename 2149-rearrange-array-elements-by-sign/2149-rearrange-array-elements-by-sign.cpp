d31025beabe0ded3fe068318faceb0a6e1f926b1
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2026-02-07 23:40:41 +0530

    Time: 2211 ms (5.37%), Space: 128.5 MB (37.85%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,17 +1,42 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>ev;
-        vector<int>od;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<0)od.push_back(nums[i]);
-            else ev.push_back(nums[i]);
+        int n=nums.size();
+        int p=0,q=0,r=0;
+        while(r<n){
+            if(r%2==0){
+                while(p<n && nums[p]<0){
+                    p++;                     
+                }
+                if(nums[p]>0){
+                    // swap(nums[p],nums[r]);
+                    int temp = nums[p];
+                    for(int k = p; k > r; k--){
+                        nums[k] = nums[k-1];   // shift instead of swap
+                    }
+                    nums[r] = temp;
+                    r++;
+                    p++;
+                    // q=p;
+                }
+            }
+            else{
+                while(q<n && nums[q]>0){
+                    q++;                     
+                }
+                if(nums[q]<0){
+                    // swap(nums[q],nums[r]);
+                    int temp= nums[q];
+                    for(int k = q; k > r; k--){
+                        nums[k] = nums[k-1];   // shift instead of swap
+                    }
+                    nums[r] = temp;
+                    r++;
+                    q++;
+                    // p=q;
+                }
+            }
         }
-        vector<int>ans;
-        for(int i=0;i<nums.size()/2;i++){
-            ans.push_back(ev[i]);
-            ans.push_back(od[i]);
-        }
-        return ans;
+        return nums;
     }
 };
