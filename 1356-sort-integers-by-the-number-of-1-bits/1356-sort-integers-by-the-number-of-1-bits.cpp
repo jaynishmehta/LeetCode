65e43b54554415683d1df9b9e55544e51727573d
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2026-02-26 00:15:31 +0530

    Time: 2 ms (52.93%), Space: 15 MB (16.23%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int cntbits(int n){
+        int cnt=0;
+        while(n!=0){
+            if((n&1)==1)cnt++;
+            n=n>>1;
+        }
+        return cnt;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        // unordered_map<int,priority_queue<int,vector<int>,greater<int>>>m;
+        map<int,vector<int>>m;
+
+        for(int i=0;i<arr.size();i++){
+            int x= cntbits(arr[i]);
+            m[x].push_back(arr[i]);
+        }
+        vector<int>v;
+        for(auto it:m){
+            // while(!it.second.empty()){
+            //     v.push_back(it.second.top());
+            //     it.second.pop();
+            // }
+            sort(it.second.begin(),it.second.end());
+            for(auto its:it.second){
+                v.push_back(its);
+            }
+        }
+        return v;
+    }
+};
