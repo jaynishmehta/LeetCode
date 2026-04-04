c90222ebe8f8d8fb8de9760c7842be519c0a487f
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2026-04-05 01:31:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    bool dfs(int node,vector<int>&v,vector<int>&p,vector<vector<int>>&adj){
-        v[node]=1;
-        p[node]=1;
-        for(auto it:adj[node]){   // checking the adjacennt node
-            if(!v[it]){
-                // p=it;
-                if(dfs(it,v,p,adj))return true;  // if 
-            }
-            else if(v[it]==1 && p[it]==1) {
-                return true;
+    bool detect(int i, vector<vector<int>>&adj,vector<int>&v){
+        v[i]=1;
+        queue<pair<int,int>>q;
+        q.push({i,-1});
+        while(!q.empty()){
+            int parent=q.front().second;
+            int node= q.front().first;
+            q.pop();
+            for(auto it:adj[i]){
+                if(!v[it]){
+                    v[it]=1;
+                    q.push({it,i});
+                }
+                else if(it!=parent){
+                    return true;
+                }
             }
         }
-        p[node]=0;
         return false;
     }
-
-    bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<vector<int>> adj(n, vector<int>());
-        // vector<int> degree(n, 0);
-        for (auto &p: pre) {
-            adj[p[1]].push_back(p[0]);
-            // degree[p[0]]++;
+    bool canFinish(int courses, vector<vector<int>>& pr) {
+        vector<vector<int>>adj(courses,vector<int>());
+        for(int i=0;i<pr.size();i++){
+            adj[pr[i][1]].push_back(pr[i][0]);
         }
-        
         vector<int>v(adj.size(),0);
-        vector<int>p(adj.size(),0);
         for(int i=0;i<adj.size();i++){
             if(!v[i]){
-                if(dfs(i,v,p,adj))return false;
+                if(detect(i,adj,v))return false;
             }
         }
         return true;
