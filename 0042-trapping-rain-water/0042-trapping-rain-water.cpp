1c2769bd6b7283e1e79ebf3694b784f2bf3634b1
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2026-04-05 01:04:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -6,12 +6,12 @@ public:
         vector<int>rightmaxi(n);
         int maxi=0;
         for(int i=0;i<n;i++){
-            leftmaxi[i]=max(maxi,static_cast<int>(height[i])); //prefix max sum 
+            leftmaxi[i]=max(maxi,height[i]);
             maxi=leftmaxi[i];
         }
         maxi=0;
         for(int i=n-1;i>=0;i--){
-            rightmaxi[i]=max(maxi,static_cast<int>(height[i]));
+            rightmaxi[i]=max(maxi,(height[i]));
             maxi=rightmaxi[i];
         }
         long long area=0;
