f872c6ef3a6dfe907f3bd45689b0947dbd46fb4c
JAYNISH MEHTA <112923627+jaynishmehta@users.noreply.github.com>
2026-04-05 17:00:01 +0530

    Time: 0 ms (100%), Space: 18.6 MB (46.39%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool dfs(int node, vector<int>& vis, vector<int>& pathVis, vector<vector<int>>& adj, stack<int>& st){
+
+        vis[node] = 1;
+        pathVis[node] = 1;
+
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                if(dfs(it, vis, pathVis, adj, st)) return true;
+            }
+            else if(pathVis[it]){
+                return true;   // cycle detected
+            }
+        }
+
+        pathVis[node] = 0;
+        st.push(node);
+
+        return false;
+    }
+    vector<int> findOrder(int courses, vector<vector<int>>& pr) {
+        vector<vector<int>> adj(courses);
+
+        for(int i = 0; i < pr.size(); i++){
+            adj[pr[i][1]].push_back(pr[i][0]);
+        }
+        
+        vector<int> vis(courses, 0);
+        vector<int> pathVis(courses, 0);
+        stack<int> st; // no need for topological arrangements in this ques
+
+        for(int i = 0; i < courses; i++){
+            if(!vis[i]){
+                if(dfs(i, vis, pathVis, adj, st)) return {};
+            }
+        }
+        vector<int>ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
